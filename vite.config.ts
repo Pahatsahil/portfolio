5a8e2bb265f53f715010464049684f467092862c
Sahil <apple1@iTM-Macbook-2.local>
2026-03-31 15:13:42 +0530

    url updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import sourceIdentifierPlugin from "vite-plugin-source-identifier";
 
-const isProd = process.env.BUILD_MODE === 'prod'
+const isProd = process.env.BUILD_MODE === "prod";
 // For GitHub Pages deployment, change 'your-username' and 'your-repo-name' to your actual GitHub details
 // Example: base: '/your-repo-name/'
-const baseUrl = process.env.GITHUB_PAGES ? '/sahil-portfolio-test/' : '/'
+const baseUrl = process.env.GITHUB_PAGES ? "/sahil-portfolio/" : "/";
 
 export default defineConfig({
   plugins: [
     react(),
     sourceIdentifierPlugin({
       enabled: !isProd,
-      attributePrefix: 'data-matrix',
+      attributePrefix: "data-matrix",
       includeProps: true,
-    })
+    }),
   ],
   base: baseUrl,
   resolve: {
@@ -23,4 +23,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
